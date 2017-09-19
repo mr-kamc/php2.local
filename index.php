c4ff2af1368a0eb8b86baf89c08956550c5f4e45
Aleksey Kalugin <mr.kamc@gmail.com>
2017-09-19 14:05:00 +0300

    +

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/autoload.php';
-
+/*
 $db = new \App\Db();
 
 
@@ -10,3 +10,13 @@ $news = \App\Models\News::findLast(2);
 include __DIR__ . '/templates/header.php';
 include __DIR__ . '/templates/content.php';
 include __DIR__ . '/templates/footer.php';
+*/
+
+$users = \App\Models\User::findAll();
+
+function sendEmail(User $user, string $message)
+{
+    echo 'Send message for ' . $user->email;
+}
+
+sendEmail($users[0], 'Hello');
